e4b7c123d05e0c8280350b1985b8e85aae8f2c6c
Kanchan Mandal <kanchan.m@appdeskservices.com>
2023-01-23 00:54:48 +0530

    Dependency Injection and project structure are be setup

---
+++ app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_rajeshmd_quotes_QuotesApplication.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.rajeshmd.quotes.QuotesApplication"
+)
+public final class _com_rajeshmd_quotes_QuotesApplication {
+}
---
+++ app/src/main/java/com/rajeshmd/quotes/QuotesApplication.kt
@@ -0,0 +1,7 @@
+package com.rajeshmd.quotes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class QuotesApplication : Application()
